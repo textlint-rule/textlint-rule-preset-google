a102f4cfc7c4bb08f06f46076d63e62697361ca9
azu <azuciao@gmail.com>
2017-10-09 18:19:37 +0900

    feat(packages): add textlint-rule-google-clause-order

---
+++ packages/textlint-rule-google-clause-order/src/textlint-rule-google-clause-order.js
@@ -0,0 +1,41 @@
+// MIT © 2017 azu
+"use strict";
+import { paragraphReporter, getTag } from "textlint-report-helper-for-google-preset";
+
+// https://developers.google.com/style/clause-order
+export const defaultMessage = "Put conditional clauses before instructions, not after.\n"
+    + "URL: https://developers.google.com/style/clause-order";
+const report = context => {
+    const dictionaries = [
+        {
+            pattern: /See (.*) for more (information|details)./,
+            replace: ({ captures }) => {
+                return `For more ${captures[1]}, see ${captures[0]}.`;
+            },
+            message: () => defaultMessage
+        },
+        {
+            pattern: /Click ([\w-]+) if you want to (.+)./,
+            replace: ({ captures }) => {
+                return `To ${captures[1]}, click ${captures[0]}.`
+            },
+            replaceTest: ({ all, captures }) => {
+                return /^VB/.test(getTag(all, captures[0]))
+            },
+            message: () => defaultMessage
+        }
+    ];
+
+    const { Syntax, RuleError, fixer, report } = context;
+    return {
+        [Syntax.Paragraph](node) {
+            paragraphReporter({
+                node, dictionaries, report, RuleError, fixer
+            });
+        }
+    };
+};
+module.exports = {
+    linter: report,
+    fixer: report
+};
