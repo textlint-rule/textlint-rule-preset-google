3a4d554d79975803cb3969d191b83d2f0ffdcb1a
azu <azu@users.noreply.github.com>
2017-10-09 18:25:57 +0900

    feat(packages): add textlint-rule-google-clause-order (#2)
    * feat(packages): add textlint-rule-google-clause-order
    
    * fix typo
    
    * Add detail

---
+++ packages/textlint-rule-google-clause-order/test/textlint-rule-google-clause-order-test.js
@@ -0,0 +1,48 @@
+const TextLintTester = require("textlint-tester");
+const tester = new TextLintTester();
+// rule
+const rule = require("../src/textlint-rule-google-clause-order");
+const defaultMessage = require("../src/textlint-rule-google-clause-order").defaultMessage;
+tester.run("textlint-rule-google-clause-order", rule, {
+    valid: [
+        "Say you want to tell the audience to do something in a particular circumstance. If possible, mention the circumstance before you provide the instruction; that way, the reader can skip the instruction if the circumstance doesn't apply.",
+        "To get the user's phone number, call `user.phoneNumber.get()`.",
+        "To clean up, call the `collectGarbage()` method.",
+        "For more information, see [link to other document].",
+        "To delete the entire document, click Delete.",
+    ],
+    invalid: [
+        {
+            text: "See link for more information.",
+            output: "For more information, see link.",
+            errors: [
+                {
+                    message: defaultMessage,
+                    index: 0
+                }
+            ],
+        },
+
+        {
+            text: "See link for more detail.",
+            output: "For more detail, see link.",
+            errors: [
+                {
+                    message: defaultMessage,
+                    index: 0
+                }
+            ],
+        },
+        {
+            text: "Click Delete if you want to delete the entire document.",
+            output: "To delete the entire document, click Delete.",
+            errors: [
+                {
+                    message: defaultMessage,
+                    index: 0
+                }
+            ],
+        }
+
+    ]
+});
