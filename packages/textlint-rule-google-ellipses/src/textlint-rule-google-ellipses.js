c3cb6dce8c90e463be8bbe67e7c99550be75b409
azu <azuciao@gmail.com>
2017-10-28 15:46:34 +0900

    feat(ellipses): Add textlint-rule-google-ellipses

---
+++ packages/textlint-rule-google-ellipses/src/textlint-rule-google-ellipses.js
@@ -0,0 +1,64 @@
+// MIT © 2017 azu
+"use strict";
+import { paragraphReporter, getPosFromSingleWord } from "textlint-report-helper-for-google-preset";
+
+const DocumentURL = "https://developers.google.com/style/ellipses";
+const report = context => {
+    const { Syntax, RuleError, fixer, report } = context;
+    const dictionaries = [
+        // NG: Suspension points
+        {
+            pattern: / \.\.\. (.*?) \.\.\. /g,
+            test: ({ captures }) => {
+                // if includes punctuation mark, ignore it
+                return !/[!?.]/.test(captures[0]);
+            },
+            message: () =>
+                "Disallow to use ellipses as suspension points." +
+                "\n" +
+                "https://developers.google.com/style/ellipses#suspension-points"
+        },
+        // NG: in beginning or end of the text.
+        {
+            pattern: /"\s*?\.\.\./g,
+            message: () =>
+                "Disallow to use ellipses in beginning of the text" +
+                "\n" +
+                "https://developers.google.com/style/ellipses#how-to-use-ellipses"
+        },
+        {
+            pattern: /\.\.\.\s*?"/g,
+            message: () =>
+                "Disallow to use ellipses in end of the text" +
+                "\n" +
+                "https://developers.google.com/style/ellipses#how-to-use-ellipses"
+        },
+        // space
+        {
+            pattern: /(\w+)\.\.\.(\w+)/g,
+            replace: ({ captures }) => {
+                return `${captures[0]} ... ${captures[1]}`;
+            },
+            message: () =>
+                "Insert one space before and after the ellipsis unless a punctuation mark immediately follows the ellipsis; in this case, don't insert a space after the ellipsis." +
+                "\n" +
+                DocumentURL
+        }
+    ];
+
+    return {
+        [Syntax.Paragraph](node) {
+            return paragraphReporter({
+                node,
+                dictionaries,
+                report,
+                RuleError,
+                fixer
+            });
+        }
+    };
+};
+module.exports = {
+    linter: report,
+    fixer: report
+};
