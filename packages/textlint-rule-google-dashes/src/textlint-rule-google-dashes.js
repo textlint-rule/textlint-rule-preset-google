497a706b69850eb142c5adeab4ed6c782905d6cf
azu <azu@users.noreply.github.com>
2017-10-28 16:01:19 +0900

    feat(ellipses): Add textlint-rule-google-ellipses (#8)
    * feat(ellipses): Add textlint-rule-google-ellipses
    
    * feat(ellipses): implement ellipses
    
    * feat(ellipses): implement ellipses
    
    * Add test

--- packages/textlint-rule-google-dashes/src/textlint-rule-google-dashes.js
+++ packages/textlint-rule-google-dashes/src/textlint-rule-google-dashes.js
@@ -1,10 +1,10 @@
 // MIT © 2017 azu
 "use strict";
-import {paragraphReporter, getPos, PosType} from "textlint-report-helper-for-google-preset";
+import { paragraphReporter, getPos, PosType } from "textlint-report-helper-for-google-preset";
 
 const DocumentURL = "https://developers.google.com/style/dashes";
 const report = context => {
-    const {Syntax, RuleError, fixer, report} = context;
+    const { Syntax, RuleError, fixer, report } = context;
     // Notes: the order is important when Apply fixes
     const dictionaries = [
         {
@@ -12,7 +12,7 @@ const report = context => {
             // Partial support:
             // use colon instead of dash or hyphen
             pattern: /((?:^.* )?(\w+)) ([—-]) ((\w+) .*)$/,
-            test: ({all, captures}) => {
+            test: ({ all, captures }) => {
                 const dashes = captures[2];
                 const afterText = captures[3];
                 // OK:
@@ -24,27 +24,34 @@ const report = context => {
                 const afterWordPos = getPos(all, afterWord);
                 // example - This is a example
                 //           ^^^^
-                if (!(afterWordPos === PosType.WhDeterminer || afterWordPos === PosType.WhPronoun || afterWordPos === PosType.Determiner)) {
+                if (
+                    !(
+                        afterWordPos === PosType.WhDeterminer ||
+                        afterWordPos === PosType.WhPronoun ||
+                        afterWordPos === PosType.Determiner
+                    )
+                ) {
                     return false;
                 }
                 const pos = getPos(all, captures[1]);
                 return pos === PosType.Noun;
             },
-            replace: ({captures}) => {
+            replace: ({ captures }) => {
                 return `${captures[0]}: ${captures[3]}`;
             },
-            message: () => "use colons(:) instead of dashes(-) in lists" + "\n" + DocumentURL
+            message: () => "Use colons(:) instead of dashes(-) in lists" + "\n" + DocumentURL
         },
         {
             // use "—"(em dash) instead of " - "(hyphen)
             // Notes: Allow to use hyphen for Ranges of numbers
             // https://developers.google.com/style/numbers#ranges-of-numbers
             pattern: /([a-zA-Z]+) - ([a-zA-Z]+)/g,
-            replace: ({captures}) => {
+            replace: ({ captures }) => {
                 return `${captures[0]}—${captures[1]}`;
             },
-            message: () => 'use "—"(em dash) instead of " - "(hyphen)' + "\n" + DocumentURL
-        }];
+            message: () => 'Use "—"(em dash) instead of " - "(hyphen)' + "\n" + DocumentURL
+        }
+    ];
 
     return {
         [Syntax.Paragraph](node) {
