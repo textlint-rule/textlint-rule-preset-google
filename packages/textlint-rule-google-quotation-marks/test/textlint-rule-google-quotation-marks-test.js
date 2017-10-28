ed76feb870eca78fce59dcb685bd5fa0fa3354b3
azu <azuciao@gmail.com>
2017-10-29 00:14:27 +0900

    Update test

--- packages/textlint-rule-google-quotation-marks/test/textlint-rule-google-quotation-marks-test.js
+++ packages/textlint-rule-google-quotation-marks/test/textlint-rule-google-quotation-marks-test.js
@@ -10,7 +10,8 @@ tester.run("textlint-rule-google-quotation-marks", rule, {
         "If you enter `foo`, the program crashes.",
         "In the latter case, put the primary speaker's quote in double quotation marks and the quote inside the primary speaker's quote in single quotation marks, in the standard American style. (This is opposite to the standard British style.)",
         'My high school English teacher made me learn that Shakespeare quote: "All the world\'s a stage, And all the men and women merely players ...."',
-        'document it as "click Save".' // <= keyword
+        'document it as "click Save".', // <= keyword,
+        `She said, "I heard him shout 'Help', and saw him floundering in the water."`
     ],
     invalid: [
         // Commas and periods with quotation marks
