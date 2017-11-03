c1216318ac4759b094e3702f6703e2f1e8bdbf5e
azu <azuciao@gmail.com>
2017-11-03 23:22:29 +0900

    feat(preset): add @textlint-rule/textlint-rule-preset-google

---
+++ packages/textlint-rule-preset-google/src/textlint-rule-preset-google.js
@@ -0,0 +1,10 @@
+// LICENSE : MIT
+"use strict";
+module.exports = {
+    rules: {
+        "google-abbreviations": require("@textlint-rule/textlint-rule-google-abbreviations")
+    },
+    rulesConfig: {
+        "google-abbreviations": true
+    }
+};
